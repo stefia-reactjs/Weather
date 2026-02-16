b4e9d9e3b45f5f4f70ec9d44b308fa0139d0f081
stefia <stefa.dxf@gmail.com>
2026-02-16 17:32:31 +0500

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter basename="/Weather">
+    <Provider store={store}>
+      <App />
+    </Provider>
+    ,
+  </BrowserRouter>,
 )
